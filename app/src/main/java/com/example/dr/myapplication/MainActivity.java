bc04783f3706ab41a1f320337f3722bf99916093
dr xumingjie <406975787@qq.com>
2016-02-17 10:26:40 +0800

    代码测试02

--- app/src/main/java/com/example/dr/myapplication/MainActivity.java
+++ app/src/main/java/com/example/dr/myapplication/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.dr.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+ private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       tv= (TextView) findViewById(R.id.dd);
+        tv.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
